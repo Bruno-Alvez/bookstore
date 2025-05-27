5ba9e06d27d2a644f1d66579c8b1add0bd220152
Bruno-Alvez <bruno.bsantos75@gmail.com>
2025-05-26 23:07:30 -0300

    feat: implement authentication using token with DRF

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "order",
     "product",
     "debug_toolbar",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
@@ -135,6 +136,11 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
